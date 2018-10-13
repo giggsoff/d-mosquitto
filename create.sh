8ef6691669fa5f5aa0da306d640ee3ad8eba5a19
Peter Fedchenkov <giggsoff@gmail.com>
2018-10-13 20:12:15 +0300

    create script

---
+++ create.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -e -p "Enter number of container:" -i "1" number
+dirname=mqtt$number
+mkdir -p "$dirname/config/"
+mkdir -p "$dirname/data/"
+mkdir -p "$dirname/log/"
+chmod 777 "$dirname/data/"
+chmod 777 "$dirname/log/"
+read -e -p "Enter config name:" -i "mosquitto_$number.conf" conf_name
+cp "$conf_name" "$dirname/config/mosquitto.conf"
+cp -r conf.d "$dirname/config/"
+read -e -p "Port1:" -i "1883" port1
+read -e -p "Port2:" -i "9001" port2
+docker run -d -p $port1:1883 -p $port2:9001 -v $PWD/$dirname/config:/mqtt/config:ro -v $PWD/$dirname/log:/mqtt/log -v $PWD/$dirname/data/:/mqtt/data/ --name mqtt$number toke/mosquitto
+docker logs -f mqtt$number
