bd8fc2fe3bcca3a64c46560c08a9b75ac217aeb5
Peter Fedchenkov <giggsoff@gmail.com>
2018-10-13 20:15:16 +0300

    save ports

--- create.sh
+++ create.sh
@@ -11,5 +11,8 @@ cp "$conf_name" "$dirname/config/mosquitto.conf"
 cp -r conf.d "$dirname/config/"
 read -e -p "Port1:" -i "1883" port1
 read -e -p "Port2:" -i "9001" port2
+echo $number > $dirname/answer
+echo $port1 >> $dirname/answer
+echo $port2 >> $dirname/answer
 docker run -d -p $port1:1883 -p $port2:9001 -v $PWD/$dirname/config:/mqtt/config:ro -v $PWD/$dirname/log:/mqtt/log -v $PWD/$dirname/data/:/mqtt/data/ --name mqtt$number toke/mosquitto
 docker logs -f mqtt$number
